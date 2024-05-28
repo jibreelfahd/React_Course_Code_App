b88cc4a51d745a933ea9edbae829a132170a9448
Fahd <jibreelfahd@gmail.com>
2024-05-28 11:38:25 +0100

    Added dynamic styles for when a user enters invlid input

--- src/components/CourseGoals/CourseForm/CourseForm.js
+++ src/components/CourseGoals/CourseForm/CourseForm.js
@@ -4,21 +4,29 @@ import "./CourseForm.css";
 
 const CourseForm = (props) => {
   const [courseData, setCourseData] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const goalChangeHandler = (event) => {
     setCourseData(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    const courseGoalData = courseData;
 
-    props.onAddGoal(courseGoalData);
+    if (courseData.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
+    props.onAddGoal(courseData);
     setCourseData("");
   };
   return (
     <form onSubmit={submitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>Course Goal</label>
         <input type="text" value={courseData} onChange={goalChangeHandler} />
       </div>
