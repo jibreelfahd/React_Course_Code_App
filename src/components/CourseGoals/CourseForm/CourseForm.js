4c3ee8a3e6caf0589db8a4e5198b648e6d5d662f
Fahd <jibreelfahd@gmail.com>
2024-05-29 01:37:34 +0100

    Added dynamic scoped styling to components using CSS Modules, and learnt how to use styled-components

--- src/components/CourseGoals/CourseForm/CourseForm.js
+++ src/components/CourseGoals/CourseForm/CourseForm.js
@@ -1,8 +1,46 @@
 import React, { useState } from "react";
 import Button from "../../UI/Button/Button";
-import "./CourseForm.css";
 
+import styles from "./CourseForm.module.css";
+
+// import styles from "styled-components";
 const CourseForm = (props) => {
+  // const FormControl = styles.div`
+  //   display: flex;
+  //   flex-direction: column;
+  //   align-items: flex-start;
+  //   justify-content: flex-start;
+
+  //   & label {
+  //   font: inherit;
+  //   font-weight: bold;
+  //   margin-bottom: 0.5rem;
+  // }
+
+  // & input {
+  //   width: 100%;
+  //   margin-bottom: 1rem;
+  //   padding: 0 0.25;
+  //   border: 1px solid #ccc;
+  //   line-height: 1.5rem;
+  // }
+
+  // & input:focus {
+  //   outline: none;
+  //   background: #fad0ec;
+  //   border-color: #8b005d;
+  // }
+
+  // &.invalid input {
+  //   background: #ffd7d7;
+  //   color: red;
+  // }
+
+  // &.invalid label {
+  //   color: red;
+  // }
+  // `;
+
   const [courseData, setCourseData] = useState("");
   const [isValid, setIsValid] = useState(true);
 
@@ -26,7 +64,7 @@ const CourseForm = (props) => {
   };
   return (
     <form onSubmit={submitHandler}>
-      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+      <div className={`${styles["form-control"]} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" value={courseData} onChange={goalChangeHandler} />
       </div>
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,39 @@
 import React from "react";
 
-import "./Button.css";
+import styles from "./Button.module.css";
+//using styled components
+// import styles from "styled-components";
+/* 
+const Button = styles.button`
+  padding: 0.5rem 1.5rem;
+  background: #8b005d;
+  border: 1px solid #8b005d;
+  cursor: pointer;
+  color: #fff;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  font: inherit;
+  width: 100%;
+
+@media (min-width: 768px) {
+  width: auto;
+}
+  
+&:focus {
+  outline: none;
+}
+
+&:hover,
+&:active {
+  background: #ac0e77;
+  border-color: #ac0e77;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+
+`; 
+*/
 
 const Button = (props) => {
   return (
-    <div className="goal__button">
+    <div className={styles.goal__button}>
       <button>{props.children}</button>
     </div>
   );
